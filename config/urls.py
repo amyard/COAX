20747b7452f2b8809c4c5ebe187534f6a22e19b4
amyard <maksymturenko@gmail.com>
2019-06-10 23:46:57 +0300

    TDD: create user and token authentication. extension - ModHeader

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/users/', include('backend.users.urls')),
+    path('api/user/', include('backend.user.urls')),
 ]
